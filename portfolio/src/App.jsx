73f97abec3d9780d32209776c5c05badbdf123db
Guilherme <guicaruaru@hotmail.com>
2025-07-02 21:18:54 -0300

    Corrigindo pasta portfolio para arquivos normais, sem submódulo

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+function App(){
+  return (
+  <>
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css" />
+    <header className="banner">
+      <img src="./imagens/banner.jpg" alt="Banner do site" />
+    </header>
+    <h1>Bem vindo ao Portfolio | </h1>
+    <h2>
+      Me chamo Guilherme Miguel | Sou Desenvolvedor Back-End | Utilizo
+      Ferramentas: Python, PostGresSQL, VBA, JavaScript
+    </h2>
+    <p>
+      Sobre mim: Sempre interessado em aprender e desenvolver, ajudo com
+      automações de serviços criando robos para soluções úteis. <br />
+      Já utilizei de desenvolvimento VBA para aplicações no SAP, realizei criação
+      de banco de dados através da utilização do PostGresSQL e criação de
+      aplicativos com Python, além de outras ferramentas com intuito de automações
+      com a linguagem.
+      <br />
+    </p>
+  </>
+  )
+}
+
+
+export default App
